4cc2839a4aecdaef189788e10d2fc48666e1f5ec
brc0d3s <brc0d3s@gmail.com>
2023-12-26 13:15:21 +0300

    C#

---
+++ Advanced/Strings/StringClone.cs
@@ -0,0 +1,11 @@
+using System;    
+    public class StringExample    
+    {    
+        public static void Main(string[] args)    
+        {    
+             string s1 = "Hello ";    
+             string s2 = (String)s1.Clone();    
+             Console.WriteLine(s1);  
+             Console.WriteLine(s2);    
+        }    
+    }    
